261c51bcf4edbac04f5952210fdb51a9e03bc79c
gozik <locoed.fan@gmail.com>
2020-02-14 00:06:58 +0300

    Update command names in controlled for Item model.

--- app/main/controller.py
+++ app/main/controller.py
@@ -8,24 +8,24 @@ class UserInterface:
     def __init__(self, user):
         self.user = user
 
-    def add_book(self, book, add_hardcopy=False):
+    def add_book(self, book, add_item=False):
         """ Add book (as description) to database """
         pass
 
     def new_item(self, item):
-        """ Add hardcopy to your collection """
+        """ Add item to your collection """
         item.owner = self.user
         item.controller = self.user
-        history = History(command='add_hardcopy', user_to=self.user, item=item)
+        history = History(command='add_item', user_to=self.user, item=item)
         db.session.add(item)
         db.session.add(history)
         db.session.commit()
 
     def take_item(self, item):
-        """ Borrow hardcopy """
+        """ Borrow item """
         user_from = item.controller
         item.controller = self.user
-        history = History(command='take_hardcopy', item=item,
+        history = History(command='take_item', item=item,
                           user_from=user_from, user_to=self.user)
         db.session.add(item)
         db.session.add(history)
@@ -39,13 +39,13 @@ class UserInterface:
         # A -> B -> C -> B (тут интерфейс должен знать, кому необходимо вернуть книгу) -> A
         last_user_from = (History.query
                             .filter_by(item=item, user_to=self.user,
-                                       command='take_hardcopy')
+                                       command='take_item')
                             .order_by(History.id.desc())
                             .first()
                      ).user_from
-        # add give hardcopy as option for command
+        # add give item as option for command
         item.controller = last_user_from
-        history = History(command='return_hardcopy', item=item,
+        history = History(command='return_item', item=item,
                           user_from=self.user, user_to=last_user_from)
         db.session.add(item)
         db.session.add(history)
@@ -53,12 +53,12 @@ class UserInterface:
 
     def give_item(self, item, user_to):
         if item.controller != self.user:
-            # you have to controll hardcopy to be able to give it
+            # you have to controll item to be able to give it
             return 1
 
         user_from = self.user
         item.controller = user_to
-        history = History(command='give_hardcopy', item=item,
+        history = History(command='give_item', item=item,
                           user_from=user_from, user_to=self.user)
         db.session.add(item)
         db.session.add(history)
